e12e2c4413bd5c90d473bec73debfe20bbd99960
Timmy <timilehinoluwaseyi07@gmail.com>
2026-06-08 18:06:33 +0100

    update server.js 2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname)));
 
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
